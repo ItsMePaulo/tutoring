1454b2df13f8c2ecde8246be4f2289919ed0dd01
Paul <itsmepaulo.wood@gmail.com>
2021-03-19 18:23:55 +0200

    Added some changes from my windows PC

--- COS212/code/u15067735/src/Main.java
+++ COS212/code/u15067735/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
+
+        System.out.println("Testing some changes");
+
         System.out.println("Hello world!");
     }
 }
